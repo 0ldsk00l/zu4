f557e4c04343d3aefae1f224fa3352cbd51bbe36
rdanbrook <1869094+rdanbrook@users.noreply.github.com>
2019-07-21 14:07:55 -0400

    Added GPL notice to savegame.c and error.c

--- src/savegame.c
+++ src/savegame.c
@@ -1,5 +1,23 @@
 /*
- * $Id: savegame.cpp 2721 2009-12-29 04:50:55Z andrewtaylor $
+ * savegame.c
+ * Copyright (C) 2009 Andrew Taylor
+ * Copyright (C) 2019 R. Danbrook
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ * 
  */
 #ifdef __cplusplus
 extern "C" {
