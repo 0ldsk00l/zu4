4cb49f7b346c03b6ef0ad562a80a2effd8794ab0
rdanbrook <1869094+rdanbrook@users.noreply.github.com>
2020-02-20 12:07:31 -0500

    Add header that was missed in previous commit

---
+++ src/video.h
@@ -0,0 +1,16 @@
+#ifndef VIDEO_H
+#define VIDEO_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void xu4_video_init();
+void xu4_video_deinit();
+void xu4_ogl_swap();
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
